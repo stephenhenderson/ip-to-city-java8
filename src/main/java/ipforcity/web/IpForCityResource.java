6e06604fb8c6d62f002cb7f74bc899e0459630ab
Stephen Henderson <stephen@magnetic.com>
2015-05-27 22:43:28 +0100

    Updated gitignore and tidying

--- src/main/java/ipforcity/web/IpForCityResource.java
+++ src/main/java/ipforcity/web/IpForCityResource.java
@@ -10,7 +10,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import com.codahale.metrics.annotation.Timed;
 
@@ -36,8 +35,6 @@ public class IpForCityResource {
 					.orElseThrow(() -> new RuntimeException("No results"));
 			return result;
 		} catch(UnknownCountryCodeException e) {
-			
-			Response r = null;
 			throw new BadRequestException(e);
 		}
 	}
